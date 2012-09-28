e5632658afb65e8bda661ecbaba36a03546dcbc3
wronex <github@wronex.com>
2012-09-28 17:03:21 +0200

    Added support for Windows

--- OpenFolder.py
+++ OpenFolder.py
@@ -5,7 +5,8 @@ class OpenFolder(sublime_plugin.WindowCommand):
 	def run(self, paths):
 		settings = sublime.load_settings("OpenFolder.sublime-settings")
 		file_manager = settings.get("file_manager", "xdg-open")
+		arg_format = settings.get("arg_format", "{0} '{1}'")
 
 		for path in paths:
 			print "Opening {0}".format(path)
-			call("{0} '{1}'".format(file_manager, path), shell=True)
+			call(arg_format.format(file_manager, path), shell=True)
