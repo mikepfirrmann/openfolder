03a828c94e77eeb5527d2ae8e1baa196f8edef28
wronex <github@wronex.com>
2012-09-28 17:16:27 +0200

    Added support for OSX

--- OpenFolder.py
+++ OpenFolder.py
@@ -4,9 +4,8 @@ from subprocess import call
 class OpenFolder(sublime_plugin.WindowCommand):
 	def run(self, paths):
 		settings = sublime.load_settings("OpenFolder.sublime-settings")
-		file_manager = settings.get("file_manager", "xdg-open")
-		arg_format = settings.get("arg_format", "{0} '{1}'")
+		file_manager = settings.get("file_manager", "xdg-open '{0}'")
 
 		for path in paths:
 			print "Opening {0}".format(path)
-			call(arg_format.format(file_manager, path), shell=True)
+			call(file_manager.format(path), shell=True)
