be3a470e1ced721f3b99d5296068b909fb5b5089
Mike Pfirrmann <mikenjss@gmail.com>
2013-03-28 11:02:59 -0400

    Fixes #3: Add Sublime Text 3 support. Also, fixes a few PEP8 compliance issues.

--- OpenFolder.py
+++ OpenFolder.py
@@ -1,11 +1,13 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 from subprocess import call
 
-class OpenFolder(sublime_plugin.WindowCommand):
-	def run(self, paths):
-		settings = sublime.load_settings("OpenFolder.sublime-settings")
-		file_manager = settings.get("file_manager", "xdg-open '{0}'")
 
-		for path in paths:
-			print "Opening {0}".format(path)
-			call(file_manager.format(path), shell=True)
+class OpenFolder(sublime_plugin.WindowCommand):
+    def run(self, paths):
+        settings = sublime.load_settings("OpenFolder.sublime-settings")
+        file_manager = settings.get("file_manager", "xdg-open '{0}'")
+
+        for path in paths:
+            print("Opening {0}".format(path))
+            call(file_manager.format(path), shell=True)
